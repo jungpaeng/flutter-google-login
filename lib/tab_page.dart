71053856cc299e83859dc4fa2c33a6575f95f72c
jungpaeng <meis1541@naver.com>
2021-03-31 16:59:56 +0900

    feat: tab page selected state

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -6,8 +6,29 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedTabIdx = 0;
+  List _pages = [Text('page1'), Text('page2'), Text('page3')];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: _pages[_selectedTabIdx]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTabIdx,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: 'Account'),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int idx) {
+    setState(() {
+      _selectedTabIdx = idx;
+    });
   }
 }
