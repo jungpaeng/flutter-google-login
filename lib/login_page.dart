650be81774919965cac2b7d4575f73d59cc8ea75
jungpaeng <meis1541@naver.com>
2021-04-01 01:30:29 +0900

    feat: login page layout

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Login Page'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Google Login Test',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            Padding(padding: EdgeInsets.all(30)),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/login_page.dart';
 import 'package:my_app/tab/tab_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
